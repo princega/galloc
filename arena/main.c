117b5ba2f875c4abb2272e53afbda7424043fe72
Prince Gautam <princegautam@protonmail.com>
2024-01-20 17:34:15 +0530

    added alloc function

--- arena/main.c
+++ arena/main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdint.h>
+#include <assert.h>
 
 typedef struct {
   size_t total;
@@ -10,14 +11,31 @@ typedef struct {
 
 Arena arena_init(size_t capacity){
   void* d = malloc(capacity*sizeof(uint8_t));
-  Arena arena {
-    .total = capacity;
-    .size = 0;
-    .data_ptr = d;
-  }
+  Arena arena = {
+    .total = capacity,
+    .size = 0,
+    .data_ptr = d,
+  };
   return arena;
 }
 
+void print(Arena* a){
+  printf("Size = %lu\nCapacity = %lu\n", a->size, a->total);
+}
+
+void* alloc(Arena* a, size_t s){
+  assert(a->size+s < a->total);
+  uint8_t data[s];
+  void* d = &data;
+  a->size += s;
+  return d;
+}
+
+void free_arena(Arena* a){
+  a->size = 0;
+  a->total = 0;
+  free(a->data_ptr);
+}
 
 int main(){
   Arena a = arena_init(1024);

66044b213d4d36b58a961e14010da280ef25abf3
Prince Gautam <princegautam@protonmail.com>
2024-02-21 18:10:25 +0530

    fixed shit

---
+++ test.c
@@ -0,0 +1,19 @@
+#include "arena/arena.c"
+#include <assert.h>
+#include <stdio.h>
+
+int main(void) {
+  uint8_t mem[1024 * 1024];
+  Arena a1 = init(mem, 7);
+  Arena a2 = init(mem + 7, 1024 * 1024 - 7);
+  int *i1 = allocate(&a1, sizeof(int));
+  int *i2 = allocate(&a2, sizeof(int));
+  puts("Write i1...");
+  *i1 = 42;
+  puts("Read i1...");
+  printf("%d\n", *i1);
+  puts("Write i2...");
+  *i2 = 42;
+  puts("Read i2...");
+  printf("%d\n", *i2);
+}
